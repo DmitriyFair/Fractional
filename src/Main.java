cf7f0ece5c74b45b207b06715db5e3e127c2959f
DmitriyFair <dmitrofair@gmail.com>
2022-08-04 20:35:17 +0300

    Use fractional number

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+     // Pythagorean theorem
+public static void main(String[] args) {
+    float a = 3.4f;
+    float b = 4.56f;
+    float c = (float) Math.sqrt(a*a+b*b);
+    System.out.println("The hypotenuse is equal to the root of the sum of the squares of the leg = " + c);
+
+}
+
+}
